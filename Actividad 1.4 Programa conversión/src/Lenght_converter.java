af0d032e77f04f67a1fc27bab78e4689b040881a
chris <chris@Chris-Lap>
2026-02-04 12:52:18 -0600

    Prueba 1

--- Actividad 1.4 Programa conversión/src/Lenght_converter.java
+++ Actividad 1.4 Programa conversión/src/Lenght_converter.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 
-public class Lenght_converter {
+public class ConvertLength {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int feet,inches;
+		float cm;
+		
+		Scanner input = new Scanner(System.in); 
+		System.out.println("Ingrese los pies: ");
+		feet = input.nextInt();
+		System.out.println("Ingrese las pulgadas: ");
+		inches = input.nextInt();
+		
+		cm= ((feet*12)+inches)*2.54;
+		
+		System.out.println("El total fue: ");
 	}
 
 }
