ae09ff96231590f034662b90df70ec14eae2088a
chris <chris@Chris_Lap>
2026-02-05 22:43:42 -0600

    Prueba tal

--- Actividad 1.5 Guess a number problem/src/ProgramaMain.java
+++ Actividad 1.5 Guess a number problem/src/ProgramaMain.java
@@ -5,7 +5,9 @@ public class ProgramaMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double num;
-		num = Math.random()*10.0;
+		num = Math.random()*100.0;
+		System.out.println("Hola");
+		
 	}
 
 }
