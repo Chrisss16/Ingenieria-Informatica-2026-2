df123b7f0f0cc6adb696126b3763852a6dd68f4c
chris <chris@TUF-Chris>
2026-03-19 09:13:44 -0600

    v0

--- Actividad 9 Clases abstractas e interfaces/src/Animal.java
+++ Actividad 9 Clases abstractas e interfaces/src/Animal.java
@@ -1,17 +1,32 @@
 
 public abstract class Animal {
-	private String nombre;
-	private int edad;
-	private float peso;
+	private String name;
+	private int age;
+	private float weight;
 	private String habitat;
-	private String cuidador;
+	private String keeper;
+	
+	public Animal() {
+		name = "Null";
+		age = 0;
+		weight = 0;
+		habitat = "Null";
+		keeper = "Null";
+	}
+	public Animal(String name1,int age1,float weight1,String habitat1,String keeper1) {
+		name = name1;
+		age = age1;
+		weight = weight1;
+		habitat = habitat1;
+		keeper = keeper1;
+	}
 	
 	public abstract void hacerSonido(); // método abstracto
 	void mostrarInfo() { // método que muestre la información del animal
-		System.out.println("Nombre: " +nombre);
-		System.out.println("Edad: " +edad);
-		System.out.println("Peso: " +peso);
+		System.out.println("Nombre: " +name);
+		System.out.println("Edad: " +age);
+		System.out.println("Peso: " +weight);
 		System.out.println("Habitad: " +habitat);
-		System.out.println("Cuidador: " +cuidador);
+		System.out.println("Cuidador: " +keeper);
 	} 
 }
---
+++ Actividad 9 Clases abstractas e interfaces/src/Fruit.java
@@ -0,0 +1,9 @@
+
+public class Fruit {
+	String name;
+	String color;
+	float weight;
+	int sweetnesslevel;
+	String countryorigin;
+	NI nutritionalinformation
+}
